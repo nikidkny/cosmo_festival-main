41f092b26066d3c9896853b4a23fbb7a6f3344d4
Nikolett Dékány <niko703j@stud.kea.dk>
2022-12-21 07:24:16 +0100

    displayname

--- components/Booking/Basket.js
+++ components/Booking/Basket.js
@@ -43,7 +43,7 @@ export default function Basket(props) {
             <div id="ticket-template">
               <div className="posts">
                 <div className="post-quantity-remove">
-                  <h4 className="turquoise type">{props.cartReg.displayname}</h4>
+                  <h4 className="turquoise type">Regular pass</h4>
                   <h5 className="white price">Ticket price: DKK {props.cartReg.price}</h5>
                 </div>
                 <div className="post-type-price">
@@ -56,7 +56,7 @@ export default function Basket(props) {
             <div id="ticket-template">
               <div className="posts">
                 <div className="post-quantity-remove">
-                  <h4 className="turquoise type">{props.cartVip.displayname}</h4>
+                  <h4 className="turquoise type">VIP pass</h4>
                   <h5 className="white price">Ticket price: DKK {props.cartVip.price}</h5>
                 </div>
                 <div className="post-type-price">

c9a1d7c363a594bd6c30642ea8ab029008b91768
Nikolett Dékány <niko703j@stud.kea.dk>
2022-12-21 07:36:32 +0100

    try 2

--- components/Booking/Basket.js
+++ components/Booking/Basket.js
@@ -43,11 +43,11 @@ export default function Basket(props) {
             <div id="ticket-template">
               <div className="posts">
                 <div className="post-quantity-remove">
-                  <h4 className="turquoise type">{props.cartReg.displayname}</h4>
-                  <h5 className="white price">Ticket price: DKK {props.cartReg.price}</h5>
+                  <h4 className="turquoise type">Regular pass</h4>
+                  <h5 className="white price">Ticket price: DKK 799</h5>
                 </div>
                 <div className="post-type-price">
-                  <h4 className="turquoise type">x{props.cartReg.amount}</h4>
+                  <h4 className="turquoise type">x</h4>
                   <button className="btn-remove">Remove</button>
                 </div>
               </div>
@@ -56,11 +56,11 @@ export default function Basket(props) {
             <div id="ticket-template">
               <div className="posts">
                 <div className="post-quantity-remove">
-                  <h4 className="turquoise type">{props.cartVip.displayname}</h4>
-                  <h5 className="white price">Ticket price: DKK {props.cartVip.price}</h5>
+                  <h4 className="turquoise type">VIP pass</h4>
+                  <h5 className="white price">Ticket price: DKK 1299</h5>
                 </div>
                 <div className="post-type-price">
-                  <h4 className="turquoise type">x{props.cartVip.amount}</h4>
+                  <h4 className="turquoise type">x</h4>
                   <button className="btn-remove">Remove</button>
                 </div>
               </div>
